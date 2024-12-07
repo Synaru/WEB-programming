2a4ed8dc7fe319c71e3bb06b1ca146f8e68d72bb
Synaru <synarutelesova@gmail.com>
2024-12-07 21:59:01 +0700

    patch

--- lab5.py
+++ lab5.py
@@ -95,7 +95,7 @@ def getlist():
     if current_app.config['DB_TYPE'] == 'postgres':
         cur.execute("SELECT * FROM articles WHERE user_id=$s;", (login_id, ))
     else:
-        cur.execute("SELECT * FROM articles WHERE user_id=?;", (login_id, ))
+        cur.execute("SELECT * FROM articles WHERE login_id=?;", (login_id, ))
     articles = cur.fetchall()
     db_close(conn, cur)
 
@@ -126,7 +126,7 @@ def create():
     if current_app.config['DB_TYPE'] == 'postgres':
         cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES ($s,$s,$s);", (login_id, title, article))
     else:
-        cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (?,?,?);", (login_id, title, article))
+        cur.execute("INSERT INTO articles (login_id, title, article_text) VALUES (?,?,?);", (login_id, title, article))
 
     db_close(conn, cur)
     return redirect('/lab5')
