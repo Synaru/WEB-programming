35a1893443dc410d808a35d004b00561a000e216
Synaru <synarutelesova@gmail.com>
2024-12-08 17:22:22 +0700

    "Бронирование офисов".

--- lab6.py
+++ lab6.py
@@ -1,12 +1,67 @@
 from flask import Blueprint, render_template, redirect, url_for, request, make_response, session, current_app
-from werkzeug.security import check_password_hash, generate_password_hash
 
 lab6 = Blueprint('lab6', __name__)
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import sqlite3
-from os import path
+
+offices = []
+for i in range(1, 11):
+    offices.append({
+        "number": i,
+        "tenant": ""
+    })
+
 
 @lab6.route("/lab6/")
 def lab6index():
     return render_template("lab6.html")
+
+
+@lab6.route("/lab6/json-rpc-api/", methods=['POST'])
+def api():
+    data = request.json
+    id = data['id']
+    if data['method'] == 'info':
+        return {
+            "jsonrpc": '2.0',
+            "result": offices,
+            'id': id
+        }
+
+    login = session.get('login')
+    if not login:
+        return {
+            "jsonrpc": '2.0',
+            "error": {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+
+    if data['method'] == 'book':
+        officeNumber = data['params']
+        for office in offices:
+            if office['number'] == officeNumber:
+                if office['tenant'] != '':
+                    return {
+                        "jsonrpc": '2.0',
+                        "error": {
+                            'code': 2,
+                            'message': 'Already booked'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = login
+                return {
+                    "jsonrpc": '2.0',
+                    "result": "success",
+                    'id': id
+                }
+
+    return {
+        "jsonrpc": '2.0',
+        "error": {
+            'code': -32601,
+            'message': 'Method not found'
+        },
+        'id': id
+    }
