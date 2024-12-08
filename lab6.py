3f948fbd489dd6da2951603fb67da61ec72c2d76
Synaru <synarutelesova@gmail.com>
2024-12-08 17:56:41 +0700

    Конец лаб 6

--- lab6.py
+++ lab6.py
@@ -1,30 +1,26 @@
 from flask import Blueprint, render_template, redirect, url_for, request, make_response, session, current_app
 
 lab6 = Blueprint('lab6', __name__)
+import random
 
 offices = []
 for i in range(1, 11):
     offices.append({
         "number": i,
-        "tenant": ""
+        "tenant": "",
+        "price": 1000 + random.randint(0, 1000) // 100 * 100
     })
 
 
 @lab6.route("/lab6/")
 def lab6index():
-    return render_template("lab6.html")
+    return render_template("lab6.html", login=session.get('login'))
 
 
 @lab6.route("/lab6/json-rpc-api/", methods=['POST'])
 def api():
     data = request.json
     id = data['id']
-    if data['method'] == 'info':
-        return {
-            "jsonrpc": '2.0',
-            "result": offices,
-            'id': id
-        }
 
     login = session.get('login')
     if not login:
@@ -34,6 +30,14 @@ def api():
                 'code': 1,
                 'message': 'Unauthorized'
             },
+            'id': id
+        }
+
+    if data['method'] == 'info':
+        return {
+            "jsonrpc": '2.0',
+            "result": offices,
+            "login": login,
             'id': id
         }
 
