254b43ca66db3b18b03d7651a338ab110dc7c28d
Synaru <synarutelesova@gmail.com>
2024-12-11 22:01:19 +0700

    «Редактирование выбранного фильма по REST».

--- lab7.py
+++ lab7.py
@@ -57,4 +57,12 @@ def delete_film(id):
     del films[id]
     return '', 204
 
+@lab7.route("/lab7/rest-api/films/<int:id>", methods=['PUT'])
+def edit_film(id):
+    if id < 0 or id > len(films) - 1:
+        return "", 404
+    film = request.get_json()
+    films[id] = film
+    return films[id]
+
 
