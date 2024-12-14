f039ff5f7f7867c8aff75a25b895e6db6d044d32
Synaru <synarutelesova@gmail.com>
2024-12-14 16:05:15 +0700

    Добавление фильма

--- lab7.py
+++ lab7.py
@@ -65,4 +65,10 @@ def edit_film(id):
     films[id] = film
     return films[id]
 
+@lab7.route("/lab7/rest-api/films/", methods=['POST'])
+def add_film():
+    film = request.get_json()
+    films.append(film)
+    return films[len(films-1)]
+
 
